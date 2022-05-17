eb271c8e70d4702975ff0d81f452d63542fff2e5
Asef Rahman <v-rahmanasef@microsoft.com>
2022-05-17 03:08:48 -0400

    Added solution for 5/14

--- Test/557.py
+++ Test/557.py
@@ -3,6 +3,7 @@ new_s = s.split(" ")
 result_s = ""
 
 for group1 in new_s:
+    # to_be_added = "".join(reversed(group1))
     to_be_added = group1[::-1]
     result_s = result_s + to_be_added + " "
 result_s = result_s.rstrip()
